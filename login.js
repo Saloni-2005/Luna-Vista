6b8111498f1e04f19e9cfa95ac0c3fcbd1f0a3df
1709RPS <rudrapratapking1709@gmail.com>
2024-12-27 00:12:12 +0530

    debugging auth
    auth had the wrong path set for index.html . (debugging auth)

--- login.js
+++ login.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
             if (storedUser && storedUser.password === password) {
                 alert("Login successful!");
-                window.location.href = "/index.html"; 
+                window.location.href = "index.html"; 
             } else {
                 alert("Invalid email or password.");
             }
