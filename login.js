6be1c652a6d47b2edd9e1af8932731091e6bae66
Saloni Kumari <chaudharysaloni2510@gmail.com>
2024-12-27 11:57:59 +0530

    Update login.js

--- login.js
+++ login.js
@@ -35,7 +35,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
             if (storedUser && storedUser.password === password) {
                 alert("Login successful!");
-               window.location.href = "./index.html"; 
+               window.location.href = "https://saloni-2005.github.io/Luna-Vista/index.html";
+
             } else {
                 alert("Invalid email or password.");
             }
